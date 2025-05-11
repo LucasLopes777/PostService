2670fd90b4273c1e1c5f1b42df931de06abee28d
LucasLopes777 <lucaslopesledok@gmail.com>
2025-05-11 13:59:41 -0300

    Primeiro commit

---
+++ src/test/java/com/algaworks/AlgaPosts/PostService/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.AlgaPosts.PostService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
